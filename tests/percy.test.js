37252a0a1d4f8a7a72bd0c7865e140d263b6e8ac
Razvan Vancea <rvancea@deloittece.com>
2020-09-09 16:07:33 +0300

    create initial test to trigger percy

---
+++ tests/percy.test.js
@@ -0,0 +1,8 @@
+import percySnapshot from "@percy/testcafe";
+
+fixture("test").page("https://qa-automation-practice.netlify.app");
+
+test("demo", async (t) => {
+  await t.wait(1000);
+  await percySnapshot(t, "homepage");
+});
