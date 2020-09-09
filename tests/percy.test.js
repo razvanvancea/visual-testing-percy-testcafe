de4b7cc874f1136cbae38bac6d18a5fd89c14b07
Razvan Vancea <rvancea@deloittece.com>
2020-09-09 18:16:33 +0300

    update percy test

--- tests/percy.test.js
+++ tests/percy.test.js
@@ -1,6 +1,7 @@
 import percySnapshot from "@percy/testcafe";
 
-fixture("test").page("https://qa-automation-practice.netlify.app");
+// prettier-ignore
+fixture `visual testing demo`.page `https://qa-automation-practice.netlify.app`;
 
 test("demo", async (t) => {
   await t.wait(1000);
